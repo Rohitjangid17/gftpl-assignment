9976885003e9dfae3c67bcb020eb3f4af17793c4
rohitjangid01 <rohit.devteam@webdaire.com>
2025-09-01 00:36:53 +0530

    logout functionality integrat

--- src/app/shared/components/layouts/sidebar/sidebar.component.ts
+++ src/app/shared/components/layouts/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -16,7 +17,20 @@ import { MatIconModule } from '@angular/material/icon';
 export class SidebarComponent {
   @Input() isOpen = true;
 
+  constructor(
+    private _authService: AuthService,
+    private _router: Router
+  ) { }
+
   logout() {
-    // Implement logout functionality
+    this._authService.logout().subscribe({
+      next: () => {
+        this._authService.clearToken();
+        this._router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.error('Logout error', err);
+      }
+    })
   }
 }
