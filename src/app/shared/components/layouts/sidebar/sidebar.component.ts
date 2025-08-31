5e91da38b2b986d48ab6878b9e5896947afc8edd
rohitjangid01 <rohit.devteam@webdaire.com>
2025-09-01 00:17:09 +0530

    sidebar ui changes

--- src/app/shared/components/layouts/sidebar/sidebar.component.ts
+++ src/app/shared/components/layouts/sidebar/sidebar.component.ts
@@ -1,14 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule, MatSidenavModule],
+  imports: [RouterModule, MatSidenavModule, MatToolbarModule, MatListModule, MatButtonModule, MatIconModule],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
   @Input() isOpen = true;
+
+  logout() {
+    // Implement logout functionality
+  }
 }
